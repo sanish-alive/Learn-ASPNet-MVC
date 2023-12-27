6c2751913e0736e7063454656581372b7e731601
mostanglan <xsonxhrestha2016@gmail.com>
2023-12-27 19:05:19 +0545

    initial commit

---
+++ Learn-ASPNet-MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Learn_ASPNet_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
