b4b42ac5db0982516ea915b8c7d5b47963dfd75c
mostanglan <xsonxhrestha2016@gmail.com>
2023-12-28 20:05:39 +0545

    controller finished

---
+++ Learn-ASPNet-MVC/Controllers/FandomController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Learn_ASPNet_MVC.Controllers
+{
+    public class FandomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NeedFandom()
+        {
+            return View("needfandom");
+        }
+    }
+}
---
+++ Learn-ASPNet-MVC/Controllers/HelloFriend.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Learn_ASPNet_MVC.Controllers
+{
+    public class HelloFriend : Controller
+    {
+        // GET: /HelloFriend
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /hellofriend/friend
+        public IActionResult Friend()
+        {
+            return View("hoho");
+        }
+
+        // GET: /hellofriend/information?name=ram&age=21
+        public string Information(string name, int age = 18)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, Age is {age}...");
+        }
+
+        // GET: /hellofriend/testing/23
+        public string Testing(int id)
+        {
+            return $"testing {id}";
+        }
+    }
+}
